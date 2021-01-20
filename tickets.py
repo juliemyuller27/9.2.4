2618bccc9afa2ee2bc4617109a0134ba5204bf31
JULIA ABROSIMOVA <75374964+juliemyuller27@users.noreply.github.com>
2021-01-20 18:27:16 +0700

    Update tickets.py

--- tickets.py
+++ tickets.py
@@ -2,7 +2,7 @@ ticket_num = int(input("Введите количество билетов "));
 tickets_price = 0;
 for i in range(ticket_num):
     temp = int(input("Введите возраст зртеля "));
-    if temp > 18:
+    if temp >= 18:
         if temp < 25:
             tickets_price = tickets_price + 990;
         else:
