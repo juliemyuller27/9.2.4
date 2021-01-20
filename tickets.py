b4ab8584cb27db273c84271464672798e6f93a66
JULIA ABROSIMOVA <75374964+juliemyuller27@users.noreply.github.com>
2021-01-20 18:06:09 +0700

    Add files via upload

---
+++ tickets.py
@@ -0,0 +1,13 @@
+ticket_num = int(input("Введите количество билетов "));
+tickets_price = 0;
+for i in range(ticket_num):
+    temp = int(input("Введите возраст зртеля "));
+    if temp > 18:
+        if temp < 25:
+            tickets_price = tickets_price + 990;
+        else:
+            tickets_price = tickets_price + 1390;
+
+if(ticket_num > 3):
+    tickets_price = tickets_price * 0.9;
+print(tickets_price);
